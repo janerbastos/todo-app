2088463342fe9e1706c4b1b27cb48910fdc9bcdf
Janer Bastos de Melo <janer.melo@ufopa.edu.br>
2019-09-11 16:37:05 -0300

    Configuraçao para suporte ao componente PageHeader

--- src/about/Abaut.jsx
+++ src/about/Abaut.jsx
@@ -1,7 +1,21 @@
 import React from 'react'
+import PageHeader from '../templates/PageHeader'
 
 export default props => (
     <div>
-        <h1>Sobre</h1>
+        <PageHeader name='Sobre' small='Nós' />
+        <br/>
+        <h3 className="display-4">História</h3>
+        <p>
+        Apparently we had reached a great height in the atmosphere, for the sky was a dead black, and the stars had ceased to twinkle. By the same illusion which lifts the horizon of the sea to the level of the spectator on a hillside, the sable cloud beneath was dished out, and the car seemed to float in the middle of an immense dark sphere, whose upper half was strewn with silver. Looking down into the dark gulf below, I could see a ruddy light streaming through a rift in the clouds
+        </p>
+        <h3 className="display-4">Missão e Visão</h3>
+        <p>
+        Apparently we had reached a great height in the atmosphere, for the sky was a dead black, and the stars had ceased to twinkle. By the same illusion which lifts the horizon of the sea to the level of the spectator on a hillside, the sable cloud beneath was dished out, and the car seemed to float in the middle of an immense dark sphere, whose upper half was strewn with silver. Looking down into the dark gulf below, I could see a ruddy light streaming through a rift in the clouds
+        </p>
+        <h3 className="display-4">Valores</h3>
+        <p>
+        Apparently we had reached a great height in the atmosphere, for the sky was a dead black, and the stars had ceased to twinkle. By the same illusion which lifts the horizon of the sea to the level of the spectator on a hillside, the sable cloud beneath was dished out, and the car seemed to float in the middle of an immense dark sphere, whose upper half was strewn with silver. Looking down into the dark gulf below, I could see a ruddy light streaming through a rift in the clouds
+        </p>
     </div>
 )
