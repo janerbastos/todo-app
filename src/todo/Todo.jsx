a9504a41aa0b605c4bb28be072bdae7a3221a80e
Janer Bastos de Melo <janer.melo@ufopa.edu.br>
2019-09-12 10:32:47 -0300

    Implementando a regra de negocio add e changer

--- src/todo/Todo.jsx
+++ src/todo/Todo.jsx
@@ -1,11 +1,34 @@
 import React, {Component} from 'react'
 import PageHeader from '../templates/PageHeader'
+import TodoForm from './TodoForm'
+import TodoList from './TodoList'
 
 export default class Todo extends Component{
+
+    constructor(props){
+        super(props)
+        this.state = {description: '', list: []}
+        this.handleAdd = this.handleAdd.bind(this)
+        this.handleChange = this.handleChange.bind(this)
+    }
+
+
+    handleChange(e){
+        this.setState({ ...this.state, description: e.target.value})
+    }
+
+    handleAdd(){
+        
+    }
+
     render(){
         return(
             <div>
                 <PageHeader name='Tarefas' small='Cadastro' />
+                <TodoForm description={this.state.description}
+                    handleAdd={this.handleAdd}
+                    handleChange={this.handleChange} />
+                <TodoList/>
             </div>
         )
     }
--- src/todo/TodoForm.jsx
+++ src/todo/TodoForm.jsx
@@ -5,9 +5,11 @@ import IconButton from '../templates/IconButton'
 export default props => (
     <div className='todoForm form-group' role='form'>
         <Grid cols='12 9 10'>
-            <imput id="description" className="form-control"
-                placeholder='Adicione uma tarefa' >
-            </imput>
+            <input type='text' id="description" className="form-control"
+                placeholder='Adicione uma tarefa'
+                onChange={props.handleChange}
+                value={ props.description }>
+            </input>
         </Grid>
         <Grid cols='12 3 2'>
             <IconButton style='primary' icon='plus'
