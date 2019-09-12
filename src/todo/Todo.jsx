28bcd781bc95c8e4fdd3758a59c42dc39f85990e
Janer Bastos de Melo <janer.melo@ufopa.edu.br>
2019-09-12 11:00:35 -0300

    Integraçao com o backend

--- src/todo/Todo.jsx
+++ src/todo/Todo.jsx
@@ -1,7 +1,11 @@
 import React, {Component} from 'react'
+import axios from 'axios'
+
 import PageHeader from '../templates/PageHeader'
 import TodoForm from './TodoForm'
 import TodoList from './TodoList'
+
+const URL = 'http://localhost:3001/todos'
 
 export default class Todo extends Component{
 
@@ -10,15 +14,22 @@ export default class Todo extends Component{
         this.state = {description: '', list: []}
         this.handleAdd = this.handleAdd.bind(this)
         this.handleChange = this.handleChange.bind(this)
+        this.refresh()
     }
 
+    refresh(){
+        axios.get(`${URL}?sort=-createAt`)
+            .then(resp => this.setState({...this.state, description: '', list: resp.data}))
+    }
 
     handleChange(e){
         this.setState({ ...this.state, description: e.target.value})
     }
 
     handleAdd(){
-        
+        const description = this.state.description
+        axios.post(URL, {description, done: 'False' })
+            .then(resp => this.refresh())
     }
 
     render(){
@@ -28,7 +39,7 @@ export default class Todo extends Component{
                 <TodoForm description={this.state.description}
                     handleAdd={this.handleAdd}
                     handleChange={this.handleChange} />
-                <TodoList/>
+                <TodoList list={this.state.list}/>
             </div>
         )
     }
