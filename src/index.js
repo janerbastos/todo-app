d5fa4a3fef3f039fc8f6a493b68ea90f88faf011
Janer Bastos de Melo <janer.melo@ufopa.edu.br>
2019-09-13 16:27:12 -0300

    Aplicaçao do middleware Redux-Multi

--- src/index.js
+++ src/index.js
@@ -2,14 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { createStore, applyMiddleware } from 'redux'
 import { Provider } from 'react-redux';
+
 import promise from 'redux-promise'
+import multi from 'redux-multi'
+import thunk from 'redux-thunk'
 
 import App from './main/App'
 import reducers from './main/reducers'
 
 import * as serviceWorker from './serviceWorker';
 
-const store = applyMiddleware(promise)(createStore)(reducers)
+const store = applyMiddleware(thunk, multi, promise)(createStore)(reducers)
 
 ReactDOM.render(
     <Provider store={store}>
