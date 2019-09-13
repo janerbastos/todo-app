c73b16acda9a00cb1c7c8001a214e91c80eaf9b4
Janer B. Melo <janerbastos@gmail.com>
2019-09-12 23:45:56 -0300

    Configurando o redux

--- src/index.js
+++ src/index.js
@@ -1,9 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore } from 'redux'
+import { Provider } from 'react-redux';
 
 import App from './main/App'
+import reducers from './main/reducers'
+
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(reducers)
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>,
+    document.getElementById('root'));
 
 serviceWorker.unregister();
